fec60ff8057243355e2269b48e77cfd2a49750b8
bobotjones <erinptacek@gmail.com>
2013-03-21 16:05:28 -0500

    Comments and stuff for clarifications.

--- src/yamahago/yamaha/yamaha.go
+++ src/yamahago/yamaha/yamaha.go
@@ -1,112 +1,116 @@
 package main
 
+/*
+A small command line utility for controlling the Yamaha RX-A720 in my living room.
+*/
+
 import (
-	"fmt"
-	"log"
-	"flag"
-	"net/http"
-	"net/url"
-	"bytes"
+  "bytes"
+  "flag"
+  "fmt"
+  "log"
+  "net/http"
+  "net/url"
 )
 
 var yHost, fileName, command string
 
 type RequestBody struct {
-	*bytes.Buffer
+  *bytes.Buffer
 }
 
 type Header struct {
-	Heads map[string]string
-	content_length	int64
+  Heads          map[string]string
+  content_length int64
 }
 
-func init () {
-	flag.StringVar(&yHost, "h", yHost, "yamaha:port")
-	flag.StringVar(&command, "c", command, "up or down")
+func init() {
+  flag.StringVar(&yHost, "h", yHost, "yamaha:port")
+  flag.StringVar(&command, "c", command, "up, down, mute, on, off or a 3-digit number to set decibels")
 }
 
-func (rb *RequestBody) Close() error {return nil}
+func (rb *RequestBody) Close() error { return nil }
 
 func pull(t url.URL, r chan *http.Response) {
-	response, err := http.Get(t.String())
-	if err != nil {
-		log.Fatal(err)
-	}
-	r <- response
+  response, err := http.Get(t.String())
+  if err != nil {
+    log.Fatal(err)
+  }
+  r <- response
 }
 
 func push(t url.URL, body *RequestBody, header Header, r chan *http.Response) {
-	req, err := http.NewRequest("POST", t.String(), body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for k, v := range(header.Heads){
-		req.Header.Set(k, v)
-	}
-	req.ContentLength = header.content_length
+  req, err := http.NewRequest("POST", t.String(), body)
+  if err != nil {
+    log.Fatal(err)
+  }
+  for k, v := range header.Heads {
+    req.Header.Set(k, v)
+  }
+  req.ContentLength = header.content_length
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	r <- resp
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  if err != nil {
+    log.Fatal(err)
+  }
+  r <- resp
 }
 
 func makeHeader(cl int64) Header {
-	w := map[string]string{
-		"User-Agent": "AV_Controller/1.0 (iOS)",
-		"SOAPACTION": "\"urn:schemas-upnp-org:service:AVTransport:1#GetTransportInfo\"",
-		"Content-Type": "text/xml; charset=\"utf-8\"",
-		"Accept-Language": "en-us",
-		"Accept-Encoding": "gzip, deflate",
-		"Connection": "keep-alive",
-		"Proxy-Connection": "keep-alive",
-	}
-	h := Header{Heads: w, content_length: cl}
-	return h
+  w := map[string]string{
+    "User-Agent":       "AV_Controller/1.0 (iOS)",
+    "SOAPACTION":       "\"urn:schemas-upnp-org:service:AVTransport:1#GetTransportInfo\"",
+    "Content-Type":     "text/xml; charset=\"utf-8\"",
+    "Accept-Language":  "en-us",
+    "Accept-Encoding":  "gzip, deflate",
+    "Connection":       "keep-alive",
+    "Proxy-Connection": "keep-alive",
+  }
+  h := Header{Heads: w, content_length: cl}
+  return h
 }
 
-func main () {
-	flag.Parse()
-	target := ""
-	rChannel := make(chan *http.Response)
-	actionPath := "/YamahaRemoteControl/ctrl"
-	buf := bytes.NewBuffer(make([]byte, 0))
-	b := make([]byte, 0)
-		
-	if yHost != "" {
-		target = "http://" + yHost + actionPath
-	} else {
-		//for testing with a proxy
-		target = "http://127.0.0.1:8080" + actionPath
-	}
-	u, err := url.Parse(target)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	switch command {
-	case "up":
-		b = volumeUp()
-	case "down":
-		b = volumeDown()
-	case "mute":
-		b = toggleMute()
-	case "on":
-		b = powerOn()
-	case "off":
-		b = standBy()
-	default:
-		// oh noes. this could be bad.
-		b = volumeAt(command)
-	}	
-	
-	buf.Write(b)	
-	header := makeHeader(int64(len(b)))
-	body := &RequestBody{buf}
-	
-	go push(*u, body, header, rChannel)
-	response := <- rChannel
-	fmt.Println(response.Status)
+func main() {
+  flag.Parse()
+  target := ""
+  rChannel := make(chan *http.Response)
+  actionPath := "/YamahaRemoteControl/ctrl"
+  buf := bytes.NewBuffer(make([]byte, 0))
+  b := make([]byte, 0)
+
+  if yHost != "" {
+    target = "http://" + yHost + actionPath
+  } else {
+    //for testing with a proxy
+    target = "http://127.0.0.1:8080" + actionPath
+  }
+  u, err := url.Parse(target)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  switch command {
+  case "up":
+    b = volumeUp()
+  case "down":
+    b = volumeDown()
+  case "mute":
+    b = toggleMute()
+  case "on":
+    b = powerOn()
+  case "off":
+    b = standBy()
+  default:
+    // oh noes. this could be bad.
+    b = volumeAt(command)
+  }
+
+  buf.Write(b)
+  header := makeHeader(int64(len(b)))
+  body := &RequestBody{buf}
+
+  go push(*u, body, header, rChannel)
+  response := <-rChannel
+  fmt.Println(response.Status)
 }
